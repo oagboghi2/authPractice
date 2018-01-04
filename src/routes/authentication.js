11b1b75a6117954c0b6b6731147d4a872a91c0fc
TeqNickels <debrenamcewen@gmail.com>
2018-01-04 14:56:37 -0800

    adds sign up functionality to app

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+const { register, getUserByEmail } = require('../../models/db')
 const app = express();
 require('ejs')
 
@@ -16,6 +19,30 @@ app.get('/', (request, response) => {
 
 app.get('/signup', (request, response) => {
   response.render('../views/signup')
+})
+
+app.get('/login', (request, response) => {
+  response.render('login', { message: null })
+})
+
+app.post('/login', (request, response) => {
+  response.render('login', {message: null})
+})
+
+app.post('/signup', (request, response) => {
+  const { name } = request.body
+  const { email } = request.body
+  const { password } = request.body
+
+  getUserByEmail(email)
+    .then((user) => {
+      if(user) {
+        response.render('login', {message: 'User already exists, please login'} )
+      } else {
+        register(name, email, password)
+        response.send('You are signed up')
+      }
+    }).catch(console.error) 
 })
 
 module.exports = app;
